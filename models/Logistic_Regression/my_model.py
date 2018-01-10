d23e7804f133ba982dd7d37640ea9029d4204639
Anjali Mittu <Anjali.Mittu@capitalone.com>
2018-01-10 11:15:05 -0500

    trying dummy vars with log regres

--- models/Logistic_Regression/my_model.py
+++ models/Logistic_Regression/my_model.py
@@ -6,7 +6,7 @@ import data_cleanup as dc
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-(train, test, ids) = dc.get_data()
+(train, test, ids) = dc.get_dummy_data()
 
 num_test = 0.20
 X_all = train.drop("Survived", axis=1)
@@ -15,11 +15,15 @@ X_train, X_dev, Y_train, Y_dev = train_test_split(X_all, Y_all, test_size=num_te
 X_test  = test
 
 # Starting with a simple log regresssion
-logreg = LogisticRegression()
-logreg.fit(X_train, Y_train)
-Y_pred = logreg.predict(X_test)
-acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
-acc_log
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+acc_random_forest = round(model.score(X_train, Y_train) * 100, 2)
+acc_random_forest
+acc_random_forest = round(model.score(X_dev, Y_dev) * 100, 2)
+acc_random_forest
+#train model on all the data
+model.fit(X_all, Y_all)
+Y_pred = model.predict(X_test)
 
 coeff_df = pd.DataFrame(X_train.columns)
 coeff_df.columns = ['Feature']
