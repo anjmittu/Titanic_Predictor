7d8062d2b4662f2c4ca34bd4fbe8a3d0cdf445a8
Anjali Mittu <Anjali.Mittu@capitalone.com>
2018-01-12 15:36:23 -0500

    Run with less features

--- data_explore.py
+++ data_explore.py
@@ -1,4 +1,5 @@
 # data analysis and wrangling
+import pandas as pd
 import data_cleanup as dc
 
 # visualization
@@ -6,10 +7,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 get_ipython().magic('matplotlib inline')
 
-(train, test) = dc.get_data()
+raw_train = pd.read_csv('data/train.csv')
+raw_test = pd.read_csv('data/test.csv')
+
+(train, test, ind) = dc.get_data()
 
 # Embarked could be useful
-train[['Fare', 'Survived']].groupby(['Fare'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+raw_train[['Cabin', 'Survived']].groupby(['Cabin'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+train[['CabinLetter', 'Survived']].groupby(['CabinLetter'], as_index=False).mean().sort_values(by='Survived', ascending=False)
 
 
 grid = sns.FacetGrid(train, col='Survived', row='CabinLetter', size=2.2, aspect=1.6)
